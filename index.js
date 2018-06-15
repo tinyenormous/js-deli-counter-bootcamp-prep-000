f0726497525910f3679ec792fbbc22c7bf0fdfc1
rossdaly@gmail.com daly <rossdaly@gmail.com>
2018-06-15 02:06:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,14 +3,8 @@ var i=0
 var returnString = ""
 function takeANumber(katzDeliLine,name) {
   console.log(katzDeliLine) 
-  do {
-    if (name === katzDeliLine[i]) {
-      returnString = `Welcome, ${name}. You are number ${i+1} in line.`
-      console.log(returnString)
-      katzDeli.push(name)
-      
-    } 
-    i++
-  } while (i<katzDeliLine.length)
+  returnString = `Welcome, ${name}. You are number ${i+1} in line.`
+  console.log(returnString)
+  katzDeli.push(name)
   return returnString
 }
